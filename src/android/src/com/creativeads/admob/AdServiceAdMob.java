dcd03e9474175d4d9ad58b4a27d16f59152bba2d
llapps <lauhoffo@gmail.com>
2020-12-02 14:07:21 +0100

    Update AdServiceAdMob.java

--- src/android/src/com/creativeads/admob/AdServiceAdMob.java
+++ src/android/src/com/creativeads/admob/AdServiceAdMob.java
@@ -68,7 +68,7 @@ public class AdServiceAdMob implements AdService {
             
             //MediationTestSuite.addTestDevice("9B45C6730D606E8951D176909258C415"); 
             //MediationTestSuite.launch(activity);
-            AudienceNetworkInitializeHelper.initialize(activity);
+            //AudienceNetworkInitializeHelper.initialize(activity);
             _initialized = true;
           
         }
