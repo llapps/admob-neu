8580e2127b554693c71fa45308db971c0d9a586c
llapps <lauhoffo@gmail.com>
2020-12-02 17:22:17 +0100

    Update AdServiceAdMob.java

--- src/android/src/com/creativeads/admob/AdServiceAdMob.java
+++ src/android/src/com/creativeads/admob/AdServiceAdMob.java
@@ -72,16 +72,8 @@ public class AdServiceAdMob implements AdService {
             AudienceNetworkAds.initialize(activity);
            // MobileAds.initialize(activity, appId);
             MobileAds.initialize(activity, new OnInitializationCompleteListener() {
-                AdapterStatus statusMap = initializationStatus.getAdapterStatusMap();
-                for (String adapterClass : statusMap.keySet()) {
-                    AdapterStatus status = statusMap.get(adapterClass);
-                    Log.d("MyApp", String.format(
-                            "Adapter name: %s, Description: %s, Latency: %d",
-                            adapterClass, status.getDescription(), status.getLatency()));
-                }
-
-             //   @Override
-             //   public void onInitializationComplete(InitializationStatus initializationStatus) {}
+                @Override
+                public void onInitializationComplete(InitializationStatus initializationStatus) {}
             });
 
             
