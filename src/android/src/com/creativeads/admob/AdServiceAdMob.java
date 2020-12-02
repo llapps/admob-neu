973ce0ce53b31821f2e160b83c4bac60de2c4635
llapps <lauhoffo@gmail.com>
2020-12-02 14:03:52 +0100

    Update AdServiceAdMob.java

--- src/android/src/com/creativeads/admob/AdServiceAdMob.java
+++ src/android/src/com/creativeads/admob/AdServiceAdMob.java
@@ -5,9 +5,6 @@ import android.content.Context;
 import android.util.Log;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.AdapterStatus;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 //import com.google.android.ads.mediationtestsuite.MediationTestSuite;
 import com.creativeads.AdBanner;
@@ -67,25 +64,11 @@ public class AdServiceAdMob implements AdService {
           //  appOptions.setGDPRRequired(true);
            // AudienceNetworkAds.initialize(activity);
 
-           // MobileAds.initialize(activity, appId);
-            MobileAds.initialize(activity, appId, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-                Map<String, AdapterStatus> statusMap = initializationStatus.getAdapterStatusMap();
-                for (String adapterClass : statusMap.keySet()) {
-                    AdapterStatus status = statusMap.get(adapterClass);
-                    Log.d("MyApp", String.format(
-                            "Adapter name: %s, Description: %s, Latency: %d",
-                            adapterClass, status.getDescription(), status.getLatency()));
-                }
-
-                // Start loading ads here...
-            }
-        });
-
+            MobileAds.initialize(activity, appId);
+            
             //MediationTestSuite.addTestDevice("9B45C6730D606E8951D176909258C415"); 
             //MediationTestSuite.launch(activity);
-            //AudienceNetworkInitializeHelper.initialize(activity);
+            AudienceNetworkInitializeHelper.initialize(activity);
             _initialized = true;
           
         }
