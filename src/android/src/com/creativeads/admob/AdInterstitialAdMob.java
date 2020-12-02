bfea2f5bd65e5e1ad0c364c69124e75a361755b4
llapps <lauhoffo@gmail.com>
2020-12-02 13:45:11 +0100

    Update AdInterstitialAdMob.java

--- src/android/src/com/creativeads/admob/AdInterstitialAdMob.java
+++ src/android/src/com/creativeads/admob/AdInterstitialAdMob.java
@@ -6,6 +6,10 @@ import com.google.ads.mediation.admob.AdMobAdapter;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 import com.creativeads.AbstractAdInterstitial;
 
 public class AdInterstitialAdMob extends AbstractAdInterstitial {
