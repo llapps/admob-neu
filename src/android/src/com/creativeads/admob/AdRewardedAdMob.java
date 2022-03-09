17eee3c1a5c70e17b1998a7dcac7c330ad030791
llapps <lauhoffo@gmail.com>
2022-03-09 13:10:55 +0100

    Update AdRewardedAdMob.java

--- src/android/src/com/creativeads/admob/AdRewardedAdMob.java
+++ src/android/src/com/creativeads/admob/AdRewardedAdMob.java
@@ -6,9 +6,9 @@ import android.util.Log;
 import com.google.ads.mediation.admob.AdMobAdapter;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.reward.RewardItem;
-import com.google.android.gms.ads.reward.RewardedVideoAd;
-import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedVideoAd;
+import com.google.android.gms.ads.rewarded.RewardedVideoAdListener;
 import com.creativeads.AbstractAdRewardedVideo;
 import com.creativeads.AdRewardedVideo;
 
